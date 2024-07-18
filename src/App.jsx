0d63380b6364d916cbb970706941cd10ec9f6b80
filmList_new <95223859+Nadina00@users.noreply.github.com>
2024-07-18 20:43:12 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-
 import "./App.css";
 import { FormAddClient } from "./components/formAddClient/FormAddClient";
 import { Header } from "./components/header/Header";
 import { ListClient } from "./components/listClient/ListClient";
 import { useSelector } from "react-redux";
-import { department } from "./redax/form-select";
+import { department, loading } from "./redax/form-select";
 import { ToastContainer, toast } from "react-toastify";
 import { errorClient } from "../src/redax/form-select";
 import "react-toastify/dist/ReactToastify.css";
+import { Loader } from "./components/loader/Loader";
 
 function App() {
   const departmentNum = useSelector(department);
   const errorAdd = useSelector(errorClient);
-  console.log(departmentNum)
+  const loader = useSelector(loading);
+  console.log(departmentNum);
 
   const notify = () => {
     if (errorAdd) {
@@ -24,10 +25,11 @@ function App() {
   return (
     <>
       <Header />
-      {(departmentNum !== "Головний офіс") && (departmentNum !== "charge") &&<FormAddClient/> }
-      <ListClient notify={notify}/>
-      <ToastContainer/>
-      
+      {departmentNum !== "Головний офіс" && departmentNum !== "charge" && (
+        <FormAddClient />
+      )}
+      {loader ? <Loader /> : <ListClient notify={notify} />}
+      <ToastContainer />
     </>
   );
 }
--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,12 +1,13 @@
 import { Oval } from "react-loader-spinner";
 
+
 export const Loader = () => {
   return (
     <Oval
       visible={true}
-      height="80"
-      width="80"
-      color="#4fa94d"
+      height="100"
+      width="100"
+      color="rgb(97, 95, 139)"
       ariaLabel="oval-loading"
       wrapperStyle={{}}
       wrapperClass=""
